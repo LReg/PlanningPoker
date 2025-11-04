d297ab2763de153451dfb5764b3eff0d7be3a626
Nils <lreg0@web.de>
2025-11-04 13:57:53 +0100

    fix personentage

--- frontend/src/models/Session.model.ts
+++ frontend/src/models/Session.model.ts
@@ -1,23 +1,17 @@
-import type {ExportEstimatePlayer, Player} from "@/models/Player.model";
-
-export interface Session {
-    token: string;
-    name: string;
-    players: Player[];
-    // open = true -> everyone can see the estimates
-    open: boolean;
-}
+import type { ExportEstimatePlayer, ExportPlayer, Player } from '@/models/Player.model';
 
 export enum EstimationOption {
     Fibonacci = 'Fibonacci',
     PowersOfTwo = 'PowersOfTwo',
     TShirtSizes = 'TShirtSizes',
+    PersonDays = 'PersonDays',
     Custom = 'Custom',
 }
 
 export const FibonacciEstimationValues = ['🤷‍♂️', '☕', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '144'];
 export const PowersOfTwoEstimationValues = ['🤷‍♂️', '☕', '1', '2', '4', '8', '16', '32', '64', '128', '256', '512', '1024'];
 export const TShirtSizesEstimationValues = ['🤷‍♂️', '☕', 'XS', 'S', 'M', 'L', 'XL', 'XXL'];
+export const PersonDaysEstimationValues = ['🤷‍♂️', '☕', '0.5', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'];
 
 export function getEstimationValues(options: EstimationOption): string[] {
     switch (options) {
@@ -27,6 +21,8 @@ export function getEstimationValues(options: EstimationOption): string[] {
             return PowersOfTwoEstimationValues;
         case EstimationOption.TShirtSizes:
             return TShirtSizesEstimationValues;
+        case EstimationOption.PersonDays:
+            return PersonDaysEstimationValues;
         case EstimationOption.Custom:
             return [];
     }
@@ -39,19 +35,43 @@ export function parseEstimationType(type: string): EstimationOption | undefined 
         case 'PowersOfTwo':
             return EstimationOption.PowersOfTwo;
         case 'TShirtSizes':
-            return EstimationOption.TShirtSizes;
+            return EstimationOption.TShirtSizes
+        case 'PersonDays':
+            return EstimationOption.PersonDays;
         case 'Custom':
             return EstimationOption.Custom;
     }
     return undefined;
 }
 
+export interface Session {
+    token: string;
+    name: string;
+    players: Player[];
+    // open = true -> everyone can see the estimates
+    open: boolean;
+    estimationOptions: EstimationOption;
+    estimationValues: string[];
+    timeoutId?: NodeJS.Timeout;
+}
+
+export interface NewSessionDto {
+    name: string;
+    leaderName: string;
+}
+
+export interface ExportSession {
+    token: string;
+    name: string;
+    players: ExportPlayer[];
+    open: boolean;
+}
 
 export interface ExportEstimateSession {
     token: string;
     name: string;
     players: ExportEstimatePlayer[];
+    open: boolean;
     estimationOptions: EstimationOption;
     estimationValues: string[];
-    open: boolean;
 }
